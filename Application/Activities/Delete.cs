a5256b02f7707b0b98e89943c95ad76fde6542d4
shervinmontajam <montajam@gmail.com>
2019-09-28 02:42:50 +0330

    Query and command added

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+
+            public Handler(DataContext dataContext)
+            {
+                _dataContext = dataContext;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _dataContext.Activities.FindAsync(request.Id);
+                if(activity == null) throw new Exception("Not Found");
+
+                _dataContext.Activities.Remove(activity);
+
+                if (await _dataContext.SaveChangesAsync(cancellationToken) > 0)
+                    return Unit.Value;
+
+                throw new Exception("Problem in saving changes!");
+            }
+        }
+    }
+}
